cc155177eaad823f5758779bc49a9a9198da0d26
jredrado <56800657+jredrado@users.noreply.github.com>
2023-09-30 13:25:47 +0200

    Update encode.rs

--- minicbor/src/encode.rs
+++ minicbor/src/encode.rs
@@ -172,20 +172,6 @@ impl Encode for core::num::NonZeroUsize {
 }
 
 #[cfg(target_pointer_width = "32")]
-impl Encode for std::num::NonZeroUsize {
-    fn encode<W: Write>(&self, e: &mut Encoder<W>) -> Result<(), Error<W::Error>> {
-        e.u32(self.get() as u32)?.ok()
-    }
-}
-
-#[cfg(target_pointer_width = "64")]
-impl Encode for std::num::NonZeroUsize {
-    fn encode<W: Write>(&self, e: &mut Encoder<W>) -> Result<(), Error<W::Error>> {
-        e.u64(self.get() as u64)?.ok()
-    }
-}
-
-#[cfg(target_pointer_width = "32")]
 impl Encode for isize {
     fn encode<W: Write>(&self, e: &mut Encoder<W>) -> Result<(), Error<W::Error>> {
         e.i32(*self as i32)?.ok()
